c516e316676ee0b7c71733d557c32dc49ab03ba1
Aleisder <danyatsarenkomail.ru@gmail.com>
2024-06-17 09:21:35 +0300

    Add log classes

---
+++ Model/Log/Log.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DemoExam.Model.Log
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public Type Type { get; set; }
+        public string Module { get; set; }
+        public string Description { get; set; }
+        public DateTime LoggedAt { get; set; }
+
+        public Log(int id, Type type, string module, string description, DateTime loggedAt)
+        {
+            Id = id;
+            Type = type;
+            Module = module;
+            Description = description;
+            LoggedAt = loggedAt;
+        }
+    }
+}
---
+++ Model/Log/Type.cs
@@ -0,0 +1,16 @@
+﻿namespace DemoExam.Model.Log
+{
+    public class Type
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+
+        public Type(int id, string value)
+        {
+            Id = id;
+            Value = value;
+        }
+
+        public override string ToString() => Value;
+    }
+}
